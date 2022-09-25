a089fb82cfece35b9a29f938c870f8d54de7e1a4
Arthur Khakimov <verybigfolder@yandex.ru>
2022-09-25 19:59:27 +0500

    Update navbar

--- src/components/app.tsx
+++ src/components/app.tsx
@@ -1,25 +1,24 @@
-import { h } from 'preact'
-import { Route, Router } from 'preact-router'
-import { createHashHistory } from 'history';
+import { h } from "preact";
+import { Route, Router } from "preact-router";
+import { createHashHistory } from "history";
 
-import Header from './header'
+import Header from "./header";
 
 // Code-splitting is automated for `routes` directory
-import Home from '../routes/home'
+import Home from "../routes/home";
 import { Lab1 } from "../routes/labs/lab1";
 
-
 const App = () => {
-    const history: any =  createHashHistory()
-    return (
-        <div id="app">
-            <Header />
-            <Router history={history}>
-                <Route path="/" component={Home} />
-                <Route path="/lab/1" component={Lab1} />
-            </Router>
-        </div>
-    )
-}
+  const history: any = createHashHistory();
+  return (
+    <div id="app">
+      <Header />
+      <Router history={history}>
+        <Route path="/" component={Home} />
+        <Route path="/lab/1" component={Lab1} />
+      </Router>
+    </div>
+  );
+};
 
-export default App
+export default App;
--- src/components/header/index.tsx
+++ src/components/header/index.tsx
@@ -4,7 +4,6 @@ import style from './style.css';
 
 const Header = () => (
 	<header class={style.header}>
-		<h1>Лабораторная работы по Современным компьютерным технологиям</h1>
 		<nav>
 			<Link activeClassName={style.active} href="/lab/1">Лаба 1</Link>
 			<Link activeClassName={style.active} href="/">Home Page</Link>
