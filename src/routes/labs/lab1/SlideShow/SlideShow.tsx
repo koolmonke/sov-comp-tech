52cdfd9c3c36d92f8863ac4dac5c9a7d4c742d9c
Arthur Khakimov <verybigfolder@yandex.ru>
2022-09-25 15:46:33 +0500

    Slideshow рефактор

--- src/routes/labs/lab1/SlideShow/SlideShow.tsx
+++ src/routes/labs/lab1/SlideShow/SlideShow.tsx
@@ -6,9 +6,9 @@ export const Slideshow = ({ imageUrls }: { imageUrls: string[] }) => {
   const reducer = (currentIndex: number, action: "next" | "prev") => {
     switch (action) {
       case "next":
-        return currentIndex > 0 ? currentIndex - 1 : imageUrls.length - 1;
+        return (currentIndex + 1) % imageUrls.length;
       case "prev":
-        return currentIndex < imageUrls.length - 1 ? currentIndex + 1 : 0;
+        return currentIndex == 0 ? imageUrls.length - 1 : currentIndex - 1;
     }
   };
 
@@ -17,15 +17,15 @@ export const Slideshow = ({ imageUrls }: { imageUrls: string[] }) => {
   return (
     <div class={style.slideContainer}>
       <div class={style.slide}>
-        <a class={style.buttons} onClick={() => dispatch("next")}>
+        <a class={style.buttons} onClick={() => dispatch("prev")}>
           &#10094;
         </a>
         <img class={style.slideImage} src={imageUrls[currentIndex]} />
-        <a class={style.buttons} onClick={() => dispatch("prev")}>
+        <a class={style.buttons} onClick={() => dispatch("next")}>
           &#10095;
         </a>
       </div>
-      <p class={style.pageCount}>
+      <p>
         {currentIndex + 1}/{imageUrls.length}
       </p>
     </div>
