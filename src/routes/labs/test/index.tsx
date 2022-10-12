589707fe4d6e8d8a5322aeb1d4995fa98f85d139
Arthur Khakimov <verybigfolder@yandex.ru>
2022-10-13 00:40:11 +0500

    Remove braces

--- src/routes/labs/test/index.tsx
+++ src/routes/labs/test/index.tsx
@@ -3,18 +3,16 @@ import labStyle from "../style.css";
 import { countRightAnswer, isAllAnswered, testState } from "./TestState";
 import SingleTest from "./SingleTest";
 
-const Test = () => {
-  return (
-    <div class={labStyle.lab}>
-      {testState.value.map((item) => SingleTest(item))}
-      {isAllAnswered.value && (
-        <h3>
-          Всего правильных ответов {countRightAnswer} из{" "}
-          {testState.value.length}
-        </h3>
-      )}
-    </div>
-  );
-};
+const Test = () => (
+  <div class={labStyle.lab}>
+    {testState.value.map((item) => SingleTest(item))}
+    {isAllAnswered.value && (
+      <h3>
+        Всего правильных ответов {countRightAnswer} из{" "}
+        {testState.value.length}
+      </h3>
+    )}
+  </div>
+);
 
 export default Test;
