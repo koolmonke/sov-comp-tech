b947049fcb8cf0972be2fa8c2023b3546301a4b4
Arthur Khakimov <verybigfolder@yandex.ru>
2022-10-25 22:02:03 +0500

    Refactor css

--- src/routes/labs/square/index.tsx
+++ src/routes/labs/square/index.tsx
@@ -1,11 +1,10 @@
 import { h } from "preact";
-import labStyle from "../style.css";
 import style from "./style.css"
 
 const Square = () => {
 
   return (
-    <div class={labStyle.lab}>
+    <div class={style.container}>
      <div class={style.sun}>
       <div class={style.dot}> </div>
      </div>
