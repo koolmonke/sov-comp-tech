c7a53b29fb3e71889e18fa743af1384f9fe2e3b1
Arthur Khakimov <verybigfolder@yandex.ru>
2022-11-13 12:28:08 +0500

    Reformat

--- src/routes/labs/store/index.tsx
+++ src/routes/labs/store/index.tsx
@@ -10,18 +10,16 @@ import {
 import StoreItemComponent from "./components/StoreItem";
 import CartItemComponent from "./components/CartItem";
 
-const Cart = () => {
-  return (
-    <div class={labStyle.lab}>
-      <div class={style.store}>{storeItems.map(StoreItemComponent)}</div>
-      <div class={style.cart}>
-        <p>Корзина</p>
-        {isCartEmpty.value && <p>Ваша корзина пуста</p>}
-        {cartState.value.map(CartItemComponent)}
-        {!isCartEmpty.value && <p>Сумма корзины {totalCartPrice} руб.</p>}
-      </div>
+const Cart = () => (
+  <div class={labStyle.lab}>
+    <div class={style.store}>{storeItems.map(StoreItemComponent)}</div>
+    <div class={style.cart}>
+      <p>Корзина</p>
+      {isCartEmpty.value && <p>Ваша корзина пуста</p>}
+      {cartState.value.map(CartItemComponent)}
+      {!isCartEmpty.value && <p>Сумма корзины {totalCartPrice} руб.</p>}
     </div>
-  );
-};
+  </div>
+);
 
 export default Cart;
